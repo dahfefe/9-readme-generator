7ba5e6319194b066d3252779d45ab1a2e7cfa261
Felix <157293782+dahfefe@users.noreply.github.com>
2024-04-06 20:08:54 -0700

    ensuring functionality of inquirer prompts with format generation of README file. First two prompts successful.

--- index.js
+++ index.js
@@ -11,42 +11,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <header class="p-5 mb-4 header bg-light">
-    <div class="container">
-      <h1 class="display-4">Hi! My name is ${name}</h1>
-      <p class="lead">I am from ${location}.</p>
-      <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item">My GitHub username is ${github}</li>
-        <li class="list-group-item">LinkedIn: ${linkedin}</li>
-      </ul>
-    </div>
-  </header>
-</body>
-</html>`;
+/* description, toc, installation, usage, license, contributing, tests, questions */
+
+const generateReadme = ({ title, description }) =>
+  `# ${title}
+  
+  ## Description
+  
+  ${description}`
+;
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is the title?',
     },
     {
       type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      name: 'description',
+      message: 'Please provide a description of your README.',
     },
+    /*
     {
       type: 'input',
       name: 'hobby',
@@ -67,11 +54,13 @@ inquirer
       name: 'linkedin',
       message: 'Enter your LinkedIn URL.',
     },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    */
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+  ])
+  .then((responses) => {
+    const readMeContent = generateReadme(responses);
+
+    fs.writeFile('README.md', readMeContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
     );
   });
